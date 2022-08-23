ba4d5c8f9a7b79ea0a3056fb5f1901296fd6ade4
javadyousefi88 <110282775+javadyousefi88@users.noreply.github.com>
2022-08-23 10:46:08 +0430

    upload PanelDashboard Project on Github

---
+++ src/views/pages/login/Login.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CInputGroupText,
+  CRow,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilLockLocked, cilUser } from '@coreui/icons'
+
+const Login = () => {
+  return (
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md={8}>
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  <CForm>
+                    <h1>پنل مدیریت فروشگاه</h1>
+                    <p className="text-medium-emphasis">به حساب کاربری خود وارد شوید</p>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText>
+                        <CIcon icon={cilUser} />
+                      </CInputGroupText>
+                      <CFormInput placeholder="نام کاربری" autoComplete="username" />
+                    </CInputGroup>
+                    <CInputGroup className="mb-4">
+                      <CInputGroupText>
+                        <CIcon icon={cilLockLocked} />
+                      </CInputGroupText>
+                      <CFormInput
+                        type="password"
+                        placeholder="کلمه عبور"
+                        autoComplete="current-password"
+                      />
+                    </CInputGroup>
+                    <CRow>
+                      <CCol xs={6}>
+                        <CButton color="primary" className="px-4">
+                          ورود
+                        </CButton>
+                      </CCol>
+                    </CRow>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
+                <CCardBody className="text-center">
+                  <div>
+                    <h2>طراحی اپلیکیشن فروشگاهی مشابه دیجی کالا</h2>
+                    <hr />
+                    <h2>ساخت توسعه اپلیکیشن با React Native</h2>
+                    <hr />
+                    <h2>توسعه پنل مدیریت با React JS</h2>
+                    <hr />
+                    <h2>ساخت توسعه اپلیکیشن با React Native</h2>
+                    <hr />
+                    <h2>توسعه API با استفاده از GraphQL و NodeJS</h2>
+                    <Link to="/register">
+                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
+                        ثبت نام
+                      </CButton>
+                    </Link>
+                  </div>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  )
+}
+
+export default Login
